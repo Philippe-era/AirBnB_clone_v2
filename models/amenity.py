50b99551de03ca1154665582d1f8f35e0e47b598
= <=>
2023-09-20 16:26:43 -0700

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-'''Base model created '''
+"""Amenties place blueprint"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """amenity class created"""
+class Amenity(BaseModel, Base):
+    """Amenities of the place in check and place going forward
 
-    name = ""
-
-    def __init__(self, *args, **kwargs):
-        """assigning the amenity class to something"""
-        super().__init__(*args, **kwargs)
+    Attributes:
+        __tablename__ (str): The table containing the information
+        name (sqlalchemy String): The facility place
+        place_amenities (sqlalchemy relationship): one-many relationship
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
