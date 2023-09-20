a8e4cd44650d349af214da39cbae6573b2caf50e
= <=>
2023-09-20 16:45:46 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Base model set and ready you check"""
 import models
 from uuid import uuid4
 from datetime import datetime
@@ -12,56 +12,56 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """BASE MODEL TO BE DECLARED USING THE BASE MODEL
+    """IMplementation of the base model class
 
     Attributes:
-        id (sqlalchemy String): THE IDENTIFICATION
-        created_at (sqlalchemy DateTime): WHEN IT WAS CREATED AND ALL
-        updated_at (sqlalchemy DateTime):LAST UPDATE TO BE CHECKED 
+        id (sqlalchemy String): The id set and in control
+        created_at (sqlalchemy DateTime): the date time it was created
+        updated_at (sqlalchemy DateTime): the last time it was updated
     """
 
     id = Column(String(60), primary_key=True, nullable=False)
-    day_created = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_info = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Prototype of the base model in check
+        """Prototype of the thing
 
         Args:
-            *args (any):not used in this context
-            **kwargs (dict):pairs in the attributes in question
+            *args (any): Not used
+            **kwargs (dict): pairs in question.
         """
         self.id = str(uuid4())
-        self.day_created = self.updated_info = datetime.utcnow()
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
-                if key == "day_created" or key == "updated_info":
+                if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
 
     def save(self):
-        """information updated and finalised """
-        self.updated_info = datetime.utcnow()
+        """updated time."""
+        self.updated_at = datetime.utcnow()
         models.new_storage.new(self)
         models.new_storage.save()
 
     def to_dict(self):
-        """dictionary with information will be returned 
+        """dictionary will be return
         """
         dictionary_mine = self.__dict__.copy()
         dictionary_mine["__class__"] = str(type(self).__name__)
-        dictionary_mine["day_created"] = self.day_created.isoformat()
-        dictionary_mine["updated_info"] = self.updated_info.isoformat()
+        dictionary_mine["created_at"] = self.created_at.isoformat()
+        dictionary_mine["updated_at"] = self.updated_at.isoformat()
         dictionary_mine.pop("_sa_instance_state", None)
         return dictionary_mine
 
     def delete(self):
-        """delete the information from storage"""
+        """delete current object"""
         models.new_storage.delete(self)
 
     def __str__(self):
-        """instance returned into prospect"""
+        """Return the print/str representation of the BaseModel instance."""
         d = self.__dict__.copy()
         d.pop("_sa_instance_state", None)
         return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
